d8a8f672fc06db4bfc0cc582aa5371a829b6b2bc
Sadeghi85 <sadeghi85@hotmail.com>
2019-12-25 22:28:40 +0330

    .

---
+++ SecurityBuyCandidates/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SecurityBuyCandidates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnalyse = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SecurityName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SecurityDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MarketType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SecurityGroupTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StatusDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAnalyse
+            // 
+            this.btnAnalyse.Location = new System.Drawing.Point(525, 12);
+            this.btnAnalyse.Name = "btnAnalyse";
+            this.btnAnalyse.Size = new System.Drawing.Size(162, 23);
+            this.btnAnalyse.TabIndex = 0;
+            this.btnAnalyse.Text = "Analyse";
+            this.btnAnalyse.UseVisualStyleBackColor = true;
+            this.btnAnalyse.Click += new System.EventHandler(this.btnAnalyse_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SecurityName,
+            this.SecurityDescription,
+            this.MarketType,
+            this.SecurityGroupTitle,
+            this.Status,
+            this.StatusDescription});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1175, 376);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // SecurityName
+            // 
+            this.SecurityName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SecurityName.HeaderText = "SecurityName";
+            this.SecurityName.Name = "SecurityName";
+            this.SecurityName.ReadOnly = true;
+            // 
+            // SecurityDescription
+            // 
+            this.SecurityDescription.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SecurityDescription.HeaderText = "SecurityDescription";
+            this.SecurityDescription.Name = "SecurityDescription";
+            this.SecurityDescription.ReadOnly = true;
+            // 
+            // MarketType
+            // 
+            this.MarketType.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MarketType.HeaderText = "MarketType";
+            this.MarketType.Name = "MarketType";
+            this.MarketType.ReadOnly = true;
+            // 
+            // SecurityGroupTitle
+            // 
+            this.SecurityGroupTitle.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SecurityGroupTitle.HeaderText = "SecurityGroupTitle";
+            this.SecurityGroupTitle.Name = "SecurityGroupTitle";
+            this.SecurityGroupTitle.ReadOnly = true;
+            // 
+            // Status
+            // 
+            this.Status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // StatusDescription
+            // 
+            this.StatusDescription.HeaderText = "StatusDescription";
+            this.StatusDescription.Name = "StatusDescription";
+            this.StatusDescription.ReadOnly = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 444);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1175, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 479);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAnalyse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnalyse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SecurityName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SecurityDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MarketType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SecurityGroupTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StatusDescription;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ SecurityBuyCandidates/Form1.cs
@@ -0,0 +1,143 @@
+﻿using SecurityBuyCandidates.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecurityBuyCandidates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAnalyse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DB_BourseEntities ctx = new DB_BourseEntities();
+
+                dataGridView1.Invoke(new Action(
+                             () =>
+                             {
+                                 dataGridView1.Rows.Clear();
+                             }
+                    ));
+                
+                progressBar1.Value = 0;
+                    
+                //List<int> tmp = new List<int>() { 2236, 2148, 2271, 2420, 2520 };
+                //List<vwSecurity> Securities = ctx.vwSecurity.Where(x => tmp.Contains(x.SecurityID)).OrderBy(x => x.SecurityName).ToList();
+
+                List<vwSecurity> Securities = ctx.vwSecurity.Where(x => x.SecurityTypeID == 6).OrderBy(x => x.SecurityName).ToList();
+
+                for (int i = 0; i < Securities.Count; i++)
+                {
+                    vwSecurity Security = Securities[i];
+
+                    await Task.Run(() => AnalyseSingle(Security));
+
+              
+                    progressBar1.Value = (int)(((i + 1) * 100) / Securities.Count);
+                   
+                    
+                }
+
+                Console.WriteLine(string.Format("Done.\n"));
+
+                
+                //await Task.Run(() => progressBar1.Invoke(new Action(
+                //             () =>
+                //             {
+                //                 progressBar1.Value = 0;
+                //             }
+                //    )));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void AnalyseSingle(vwSecurity Security)
+        {
+            try
+            {
+                DB_BourseEntities ctx = new DB_BourseEntities();
+
+                List<vwSecurityHistory> PriceList = ctx.vwSecurityHistory.Where(x => x.AdjustmentTypeID == 18 && x.SecurityID == Security.SecurityID).OrderByDescending(x => x.Date).Take(365).ToList();
+
+                double wma = WMA(PriceList);
+                double percent = 0;
+                double close = PriceList[0].ClosingPrice;
+                if (wma >= close)
+                {
+                    percent = (wma - close) / close;
+                }
+                else
+                {
+                    percent = (close - wma) / wma;
+                }
+
+                if (percent <= .08)
+                {
+                    dataGridView1.Invoke(new Action(
+                             () =>
+                            {
+                                var index = dataGridView1.Rows.Add();
+                                dataGridView1.Rows[index].Cells["SecurityName"].Value = Security.SecurityName;
+                                dataGridView1.Rows[index].Cells["SecurityDescription"].Value = Security.SecurityDescription;
+                                dataGridView1.Rows[index].Cells["MarketType"].Value = Security.MarketType;
+                                dataGridView1.Rows[index].Cells["SecurityGroupTitle"].Value = Security.SecurityGroupTitle;
+                                dataGridView1.Rows[index].Cells["Status"].Value = Security.Status;
+                                dataGridView1.Rows[index].Cells["StatusDescription"].Value = Security.StatusDescription;
+                            }
+                    ));
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private double WMA(List<vwSecurityHistory> PriceList, int Period = 26, int Offset = 0)
+        {
+            double p = 0;
+
+            int j = 0;
+            int k = Period + Offset;
+            for (int i = 0 + Offset; i < k; i++)
+            {
+                if (PriceList[i].Volume > 0)
+                {
+                    p += PriceList[i].ClosingPrice * (Period - j);
+                    j++;
+                }
+                else
+                {
+                    if (k < 365)
+                    {
+                        k++;
+                    }
+                }
+            }
+
+            p = p / (Period * (Period + 1) / 2);
+
+            return p;
+        }
+    }
+}
